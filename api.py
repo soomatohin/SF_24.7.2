1d910600ff58d31e935a8519776ec537f7061877
soomatohin <klausneverdie@hotmail.com>
2024-05-31 10:36:56 +0300

    add new files

---
+++ api.py
@@ -0,0 +1,126 @@
+import json
+import requests
+
+
+class PetFriends:
+    """API библиотека к веб приложению Pet Friends"""
+
+    def __init__(self):
+        self.base_url = "https://petfriends.skillfactory.ru/"
+
+    def get_api_key(self, email: str, pswrd: str) -> json:
+        """Метод получения api ключа (get)"""
+
+        headers = {
+            'email': email,
+            'password': pswrd,
+        }
+        res = requests.get(self.base_url+'api/key', headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def get_list_of_pets(self, auth_key: json, filter: str = "") -> json:
+        """Метод получения списка питомцев (get)"""
+
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+        res = requests.get(self.base_url + 'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def add_new_pet(self, auth_key: json, name: str, animal_type: str,
+                    age: str, pet_photo: str) -> json:
+        """Метод добавления питомца (post)"""
+
+        data = {
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+            }
+        headers = {'auth_key': auth_key['key']}
+        file = {'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')}
+        res = requests.post(self.base_url + 'api/pets', headers=headers, data=data, files=file)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+    def delete_pet(self, auth_key: json, pet_id: str) -> json:
+        """Метод удаления существующего питомца (delete)"""
+
+        headers = {'auth_key': auth_key['key']}
+        res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def update_pet_info(self, auth_key: json, pet_id: str, name: str,
+                        animal_type: str, age: int) -> json:
+        """Метод обновления данных существующего питомца (put)"""
+
+        headers = {'auth_key': auth_key['key']}
+        data = {
+            'name': name,
+            'age': age,
+            'animal_type': animal_type
+        }
+        res = requests.put(self.base_url + 'api/pets/' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def add_new_pet_simple(self, auth_key: json, name: str, animal_type: str,
+                    age: str) -> json:
+        """Метод добавления питомца без фото (post)"""
+
+        data = {
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+            }
+        headers = {'auth_key': auth_key['key']}
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+    def add_pet_photo(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
+        """Метод добавления фото для существующего питомца (post)"""
+
+        headers = {'auth_key': auth_key['key']}
+        file = {'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')}
+        res = requests.post(self.base_url + 'api/pets/set_photo/' + pet_id, headers=headers, files=file)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
---
+++ tests/test_pet_friends.py
@@ -0,0 +1,257 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+import random
+import os
+
+pf = PetFriends()
+
+
+def test_api_key_get(email=valid_email, password=valid_password):
+    """Проверка на статус запроса == 200 при запросе API ключа и в ответе присутствует ключ 'key'"""
+
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 200
+    assert 'key' in result
+
+
+def test_pet_get_list(pet_filter='my_pets'):
+    """Проверка статуса запроса и самого списка своих питомцев (может быть пустым, но списком)"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, pet_filter)
+
+    assert status == 200
+
+    if not isinstance(result['pets'], list):
+        raise Exception("Элемент 'pets' не является списком.")
+
+    assert len(result['pets']) >= 0
+
+
+def test_pet_add(name='Стеклокотамбус', animal_type='Стеклокот',
+                 age='8', pet_photo='images/glasscat.jpg'):
+    """Проверка на корректное добавление питомца и изменения длины списка после добавления
+    (в сравнении со списком перед добавлением)"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+
+    assert status == 200
+    assert result['name'] == name
+    list_len_save += 1
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    assert len(result['pets']) == list_len_save
+
+
+def test_pet_update_first(name='Ужаскот', animal_type='Ужастик', age='5'):
+    """Проверка обновления информации питомца, длина списка питомцев при этом не должна меняться"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, 'my_pets')
+
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
+
+        assert status == 200
+        assert result['name'] == name
+    else:
+        raise Exception("Питомцы отсутствуют.")
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    assert len(result['pets']) == list_len_save
+
+
+def test_pet_update_photo_any(pet_photo='images/cursecat.jpg'):
+    """Проверка обновления информации питомца, длина списка питомцев при этом не должна меняться"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) > 0:
+        pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+        status, result = pf.add_pet_photo(auth_key, random.choice(my_pets['pets'])['id'], pet_photo)
+
+        assert status == 200
+    else:
+        raise Exception("Питомцы отсутствуют.")
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    assert len(my_pets['pets']) == list_len_save
+
+
+def test_pet_delete_first():
+    """Проверка удаления первого в списке своих питомцев (длина списка питомцев должна сократиться)"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) == 0:
+        raise Exception("Питомцы отсутствуют.")
+    else:
+        pet_id = my_pets['pets'][0]['id']
+        status, temp = pf.delete_pet(auth_key, pet_id)
+        assert status == 200
+        list_len_save -= 1
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+
+    assert pet_id not in my_pets.values()
+    assert len(my_pets['pets']) == list_len_save
+
+
+def test_pet_add_simple(name='Великолепнокот', animal_type='Магнификант',
+                 age='9'):
+    """Проверка на корректное добавление питомца и изменения длины списка после добавления
+    (в сравнении со списком перед добавлением)"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    status, result = pf.add_new_pet_simple(auth_key, name, animal_type, age)
+
+    assert status == 200
+    assert result['name'] == name
+    list_len_save += 1
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    assert len(result['pets']) == list_len_save
+
+
+def test_pet_update_photo_first(pet_photo='images/magnificat.jpg'):
+    """Проверка обновления информации питомца, длина списка питомцев при этом не должна меняться"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) > 0:
+        pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+        status, result = pf.add_pet_photo(auth_key, my_pets['pets'][0]['id'], pet_photo)
+
+        assert status == 200
+    else:
+        raise Exception("Питомцы отсутствуют.")
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    assert len(my_pets['pets']) == list_len_save
+
+
+def test_pet_delete_any():
+    """Проверка удаления случайного своего питомца в списке (длина списка питомцев должна сократиться)"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) == 0:
+        raise Exception("Питомцы отсутствуют.")
+    else:
+        pet_id = random.choice(my_pets['pets'])['id']
+        status, temp = pf.delete_pet(auth_key, pet_id)
+        assert status == 200
+        list_len_save -= 1
+
+    temp, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    assert pet_id not in my_pets.values()
+    assert len(my_pets['pets']) == list_len_save
+
+
+def test_api_key_get_inv_pswrd(email=valid_email):
+    """Проверка на статус запроса == 200 при запросе API ключа и в ответе присутствует ключ 'key'"""
+
+    password = '1234567890'
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 403
+
+
+def test_api_key_get_no_pswrd(email=valid_email):
+    """Проверка на статус запроса == 200 при запросе API ключа и в ответе присутствует ключ 'key'"""
+
+    password = None
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 403
+
+
+def test_api_key_get_inv_email(password=valid_password):
+    """Проверка на статус запроса == 200 при запросе API ключа и в ответе присутствует ключ 'key'"""
+
+    email = 'abc'
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 403
+
+
+def test_api_key_get_no_email(password=valid_password):
+    """Проверка на статус запроса == 200 при запросе API ключа и в ответе присутствует ключ 'key'"""
+
+    email = None
+    status, result = pf.get_api_key(email, password)
+
+    assert status == 403
+
+
+def test_pet_get_list_no_key(pet_filter='my_pets'):
+    """Проверка статуса запроса списка своих питомцев при отсутствующем auth_key"""
+
+    auth_key = {'key': None}
+    status, result = pf.get_list_of_pets(auth_key, pet_filter)
+
+    assert status == 403
+
+
+def test_pet_get_list_inv_key(pet_filter='my_pets'):
+    """Проверка статуса запроса списка своих питомцев при некорректном auth_key"""
+
+    auth_key = {'key': '12345678-9ABC-DEF0-1234-56789ABCDEF0'}
+    status, result = pf.get_list_of_pets(auth_key, pet_filter)
+
+    assert status == 403
+
+def test_pet_add_inv_name(name=None, animal_type='Некто',
+                 age='9', pet_photo='images/glasscat.jpg'):
+    """Проверка на добавление питомца с некорректным именем (тип None) и неизменностью длины списка после этого действия
+    (в сравнении со списком перед добавлением)"""
+
+    temp, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    list_len_save = len(result['pets'])
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+
+    assert status == 400
+
+    temp, result = pf.get_list_of_pets(auth_key, 'my_pets')
+    assert len(result['pets']) == list_len_save
